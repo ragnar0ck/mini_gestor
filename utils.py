dd4c7047d6e7b150f4bdfc927ff2d5b590c7fbb8
Elo Elore Re <elore@Elos-iMac.local>
2026-01-18 14:05:53 -0400

    Update Utils.py to fix resumo_mes_atual function

--- utils.py
+++ utils.py
@@ -4,28 +4,29 @@ CSV_FILE = "gastos.csv"
 
 def adicionar_gasto(data, descricao, categoria, tipo, valor):
     df = pd.read_csv(CSV_FILE)
-    novo = pd.DataFrame(
-        [[data, descricao, categoria, tipo, valor]],
-        columns=["data", "descricao", "categoria", "tipo", "valor"]
-    )
+    novo = pd.DataFrame([[data, descricao, categoria, tipo, valor]],
+                        columns=["data","descricao","categoria","tipo","valor"])
     df = pd.concat([df, novo], ignore_index=True)
     df.to_csv(CSV_FILE, index=False)
 
 def ler_gastos():
     return pd.read_csv(CSV_FILE)
 
-def resumo_total(df):
-    return df["valor"].sum()
+def resumo_mes_atual():
+    df = pd.read_csv(CSV_FILE)
+    if df.empty:
+        return 0, pd.DataFrame()
 
-def gastos_por_categoria(df):
-    return (
-        df.groupby("categoria")["valor"]
-        .sum()
-        .sort_values(ascending=False)
-    )
+    df["data"] = pd.to_datetime(df["data"])
+    mes_atual = pd.Timestamp.now().month
+    ano_atual = pd.Timestamp.now().year
 
-def gastos_por_tipo(df):
-    return (
-        df.groupby("tipo")["valor"]
-        .sum()
-    )
+    df_mes = df[
+        (df["data"].dt.month == mes_atual) &
+        (df["data"].dt.year == ano_atual)
+    ]
+
+    total = df_mes["valor"].sum()
+    por_categoria = df_mes.groupby("categoria")["valor"].sum().reset_index()
+
+    return total, por_categoria
